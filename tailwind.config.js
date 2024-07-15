e8d2e18656ec6f66310b74ef288feda9581ca214
ohno <akifumi139@gmail.com>
2024-07-15 12:43:32 +0000

    chore:テーマカラーの設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./resources/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': {
+          '100': '#B3DBD6',
+          '300': '#94B8B4',
+          '600': '#0C4842',
+        },
+        'secondary': '#3A3226',
+        'tertiary': '#C73E3A',
+      }
+    },
   },
   plugins: [],
 }
