7babdb2d9ba72ffd942298d9771574faf282ad31
ohno <akifumi139@gmail.com>
2024-08-17 04:47:08 +0000

    add: 認証確認APIを実装

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,20 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('authorization', function (Request $request) {
+    return
+        response()
+        ->json(
+            $request->user(),
+            200,
+            [],
+            JSON_UNESCAPED_UNICODE
+        );
+    // return $request->user();
+})->middleware('auth:sanctum');
 
 Route::controller(PostController::class)
     ->middleware('auth:sanctum')
